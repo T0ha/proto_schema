6801d83389e874908641ceefcf881bdf8d290ae2
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-10-13 09:57:11 +0500

    Project init

---
+++ lib/proto_schema.ex
@@ -0,0 +1,18 @@
+defmodule ProtoSchema do
+  @moduledoc """
+  Documentation for `ProtoSchema`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProtoSchema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/proto_schema_test.exs
@@ -0,0 +1,8 @@
+defmodule ProtoSchemaTest do
+  use ExUnit.Case
+  doctest ProtoSchema
+
+  test "greets the world" do
+    assert ProtoSchema.hello() == :world
+  end
+end
